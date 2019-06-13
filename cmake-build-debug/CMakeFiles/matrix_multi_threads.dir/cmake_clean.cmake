8df57b191df9637e4f037f7ffaeb22e5908bfbde
Jonathan H <jonathan.hoyos@utec.edu.pe>
2019-06-12 19:09:37 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/matrix_multi_threads.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_multi_threads.dir/main.cpp.o"
+  "matrix_multi_threads.pdb"
+  "matrix_multi_threads"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_multi_threads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
